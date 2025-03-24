1b90fa2f92226e4c9352f592a2efb65b572a8b0f
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-24 10:32:14 +0200

    Created useEffect to call API

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,15 +1,31 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const movies = [
-    { id: 1, title: "John Wick", release_date: "2020" },
-    { id: 2, title: "Terminator", release_date: "1998" },
-    { id: 3, title: "The Matrix", release_date: "1998" },
-  ];
+  // useEffect allows you to add side effects to your function or component and defines when they should run
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.error("Error fetching popular movies:", err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []); //Callback function runs whenever the dependecy array is updated
 
   const handleSearch = (e) => {
     e.preventDefault(); //prevents the default behavior of clearing search after submit form
