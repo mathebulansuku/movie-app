22b9e82ed5f41b223a0539439b94f0009d3238e4
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-24 09:31:21 +0200

    Navbar complete

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
 import Favorite from "./pages/Favorites";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main className=" main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorite />} />
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main className=" main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 export default App;
