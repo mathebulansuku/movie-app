f58a32b742e4b2bcb0bc36af238dd0cea8f5e31c
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-24 09:14:48 +0200

    Created favorite page and install react-router dom

---
+++ movie-app/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorites Movies yet</h2>
+      <p>Start adding some movies to your favorites list!</p>
+    </div>
+  );
+}
+
+export default Favorite;
