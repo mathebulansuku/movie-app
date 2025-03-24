543f3387fa4ef3e96aa0b778d2df5c6ab08421f4
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-24 12:32:24 +0200

    Created local storage to store values within our browser

--- movie-app/src/context/MovieContext.jsx
+++ movie-app/src/context/MovieContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useEffect, useContext, useEffect } from "react";
+import { createContext, useEffect, useContext, useEffect, use } from "react";
+import { useState } from "react";
 
 const MovieContext = createContext();
 
 export const useMovieContext = () => useContext(MovieContext);
 
 export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+  });
+
   return <MovieContext.Provider>{children}</MovieContext.Provider>;
 }; //Provides state to any of the components that are wrapped inside of it
